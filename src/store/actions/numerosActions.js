0862075fa4ded89c62906655db31ea5e7cca370d
Yuri <sd.rolandi@gmail.com>
2020-10-26 11:22:05 -0300

    [UPDATE] melhorias

--- src/store/actions/numerosActions.js
+++ src/store/actions/numerosActions.js
@@ -1,14 +1,15 @@
-export function alteraNumeroMinimo(novoNumero){
-    return{
-        type: 'NUM_MIN_ALTERADO',
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from './actionsTypes'
+export function alteraNumeroMinimo(novoNumero) {
+    return {
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 
 }
 
-export function alteraNumeroMaximo(novoNumero){
-    return{
-        type: 'NUM_MAX_ALTERADO',
+export function alteraNumeroMaximo(novoNumero) {
+    return {
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 
--- src/store/reducers/numerosReducers.js
+++ src/store/reducers/numerosReducers.js
@@ -1,3 +1,5 @@
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from '../actions/actionsTypes'
+
 const initialState = {
     min: 1,
     max: 10
@@ -5,13 +7,13 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
 
-            case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
